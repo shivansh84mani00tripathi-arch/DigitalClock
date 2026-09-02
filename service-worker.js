a99167ae262aea5a1a16afb01207abcff5e30d1b
shivansh84mani00tripathi-arch <shivansh84mani00tripathi@gmail.com>
2026-09-02 06:49:55 +0530

    Update service-worker.js
    Edit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digital-clock-v2";
+const CACHE_NAME = "digital-clock-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -33,9 +33,23 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
