adf299368c2041653f65fd505eb8dd3bd699cbb9
shivansh84mani00tripathi-arch <shivansh84mani00tripathi@gmail.com>
2026-09-02 06:38:29 +0530

    Update service-worker.js
    New data

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "premanand-clock-v1";
+const CACHE_NAME = "digital-clock-v2";
 
-const FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
@@ -10,25 +10,32 @@ const FILES = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-        return response;
-      });
+      return cached || fetch(event.request);
     })
   );
 });
